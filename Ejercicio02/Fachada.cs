2bcce5da7b8bd4ccf257ab2e855515de07d4921b
francolopez9 <francolopez9@gmail.com>
2017-10-18 15:08:26 -0300

    comentario de ejemplo

---
+++ Ejercicio02/Fachada.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Fachada
+    {
+        //borrar este comentario
+        public Dividir()
+        {
+            Console.WriteLine("Ingrese el valor del Dividendo: ");
+            int pDividendo = System.Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del Divisor: ");
+            int pDivisor = System.Convert.ToInt16(Console.ReadLine());
+            Matematica pMatematica = new Matematica();
+            try
+            {
+                double pResultado = pMatematica.Dividir(pDividendo, pDivisor);
+                con
+            }
+            catch
+        }
+    }
+}
---
+++ Ejercicio02/Matematica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Matematica
+    {
+        public double Dividir(int pDividendo, int pDivisor)
+        {
+            if (pDivisor == 0)
+            {
+                throw new DivisionPorCeroException("No se puede dividir por cero");
+            }
+            else
+            {
+                return pDividendo / pDivisor;
+            }
+        }
+    }
+}
